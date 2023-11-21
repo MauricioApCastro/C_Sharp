b77843d8228a10d179c516dd495d76d79ff9375a
MauricioApCastro <castromac6367@gmail.com>
2023-11-20 21:52:42 -0300

    header do programa

--- repos/Screen Sound/ConsoleApp2/Program.cs
+++ repos/Screen Sound/ConsoleApp2/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Screen Sound
+
+string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
+
+void ExibirMensagensDeBoasVindas()
+{
+    Console.WriteLine("**************************");
+    Console.WriteLine(mensagemDeBoasVindas);
+    Console.WriteLine("**************************");
+}
+ExibirMensagensDeBoasVindas();
