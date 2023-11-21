a5264aad3867e82a5193c440e1e01a8e9bf526dc
MauricioApCastro <castromac6367@gmail.com>
2023-11-21 17:18:56 -0300

    melhoria do programa

--- repos/ConsoleApp1/ConsoleApp1/Program.cs
+++ repos/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void ExibirLogo()
+{
+    Console.WriteLine("");
+}
+
+Console.WriteLine("Bem vindo ao Screen Sound!!");
