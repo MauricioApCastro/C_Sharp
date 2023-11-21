5e3e1914bf38dbac6822f698aaf13b1cf1aa24ce
MauricioApCastro <castromac6367@gmail.com>
2023-11-20 22:06:20 -0300

    menu

--- repos/Screen Sound/ConsoleApp2/Program.cs
+++ repos/Screen Sound/ConsoleApp2/Program.cs
@@ -1,11 +1,35 @@
 ﻿// Screen Sound
 
+using System.Runtime.InteropServices;
+
 string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
 
-void ExibirMensagensDeBoasVindas()
+void ExibirLogo()
 {
-    Console.WriteLine("**************************");
-    Console.WriteLine(mensagemDeBoasVindas);
-    Console.WriteLine("**************************");
+    {
+        Console.WriteLine(@"
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░");
+    }
 }
-ExibirMensagensDeBoasVindas();
+
+void ExibirOpcoesDoMenu()
+{
+    Console.WriteLine("Digite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite -1 para sair");
+}
+ExibirLogo();
+Console.WriteLine();
+Console.WriteLine(mensagemDeBoasVindas);
+Console.WriteLine();
+ExibirOpcoesDoMenu();
+
+
+
