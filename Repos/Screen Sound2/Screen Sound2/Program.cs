49232de7c32298e5ee00e32659c95b62303b2d84
MauricioApCastro <castromac6367@gmail.com>
2023-11-26 10:58:45 -0300

    menu

--- Repos/Screen Sound2/Screen Sound2/Program.cs
+++ Repos/Screen Sound2/Screen Sound2/Program.cs
@@ -12,6 +12,44 @@ void ExibirMensagemDeBoasVindas()
 ░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
 ██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
 ╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░");
-    Console.WriteLine("\n"+mensagem);
+    Console.WriteLine("\n"+mensagem+"\n");
 }
+
+//método exibir menu
+void ExibirOpcoesDoMenu()
+{
+    Console.WriteLine("Digite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite -1 para sair");
+
+    //recebe e converte escolha
+    int numEscolha = int.Parse(Console.ReadLine()!);
+
+    //switch opçao escolhida
+    switch (numEscolha)
+    {
+        case 1:
+            Console.WriteLine("Você escolheu a opção " + numEscolha);
+            break;
+        case 2:
+            Console.WriteLine("Você escolheu a opção " + numEscolha);
+            break;
+        case 3:
+            Console.WriteLine("Você escolheu a opção " + numEscolha);
+            break;
+        case 4:
+            Console.WriteLine("Você escolheu a opção " + numEscolha);
+            break;
+        case -1:
+            Console.WriteLine("Você escolheu a opção " + numEscolha);
+            break;
+
+    } 
+}
+
 ExibirMensagemDeBoasVindas();
+ExibirOpcoesDoMenu();
+
+
