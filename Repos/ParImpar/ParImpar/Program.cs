a3271281611de3f142e998587e87a0f3a493c374
MauricioApCastro <castromac6367@gmail.com>
2023-11-26 10:43:36 -0300

    finalizaçao mensagem

--- Repos/ParImpar/ParImpar/Program.cs
+++ Repos/ParImpar/ParImpar/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿List<int> numeros = new List<int>{ 1,2,3,4,5,6,7,8,9,10};
+
+foreach (int numero in numeros)
+{
+    if (numero % 2 == 0)
+    {
+        Console.WriteLine(numero);
+    }
+    
+}
---
+++ Repos/ScreenSound/ScreenSound/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("***************************");
+Console.WriteLine("Boas vindas ao Screen Sound");
+Console.WriteLine("***************************");
+
